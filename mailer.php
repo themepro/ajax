2b46c621c0a331c64c18adf7c26efb0b2fa661bb
themepro <35577973+themepro@users.noreply.github.com>
2019-09-13 02:10:22 +0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        $recipient = "tuhin0680@yahoo.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
